dbf7a59f5e6da1cedc50e838ae20f83b4007cded
Vicky Gray <vickygray26@hotmail.com>
2016-03-13 21:32:53 +0000

    user can add a new post, which cannot be blank

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
+
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
@@ -9,4 +10,10 @@ function($scope){
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4}
   ];
+
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
 }]);
