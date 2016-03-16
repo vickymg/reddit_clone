eed95fefe60ffa8f6b91321139c1fdbea1c03e5a
Vicky Gray <vickygray26@hotmail.com>
2016-03-16 22:11:37 +0000

    added rails

--- flapper-news/app/assets/javascripts/home/mainCtrl.js
+++ flapper-news/app/assets/javascripts/home/mainCtrl.js
@@ -1,25 +1,22 @@
-angular.module('flapperNews', [])
-
-.factory('posts', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
-
+angular.module('flapperNews')
 .controller('MainCtrl', [
 '$scope',
 'posts',
+
 function($scope, posts){
 
   $scope.posts = posts.posts;
-  
+
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
